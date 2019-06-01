28c77c07b1d57777848dc1b024f65d757a3193d0
Ephraim-Kamau <kephraim19@gmail.com>
2019-06-01 23:38:56 +0300

    Add changes to search functionality

--- projects/views.py
+++ projects/views.py
@@ -68,13 +68,13 @@ def search_results(request):
 
     if 'business' in request.GET and request.GET["business"]:
         search_term = request.GET.get("business")
-        searched_business = Business.search_by_title(search_term)
+        searched_businesses = Businesses.search_by_business_name(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"business": searched_business})
+        return render(request, 'search.html',{"message":message,"businesses": searched_businesses})
 
     else:
-        message = "You haven't searched for any term"
+        message = "You haven't searched for any business"
         return render(request, 'search.html',{"message":message})    
 
   
