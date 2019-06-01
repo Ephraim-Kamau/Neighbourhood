3731f6a2e040bf68fbdcc8d890d089b650a2888f
Ephraim-Kamau <kephraim19@gmail.com>
2019-06-01 12:49:11 +0300

    Add template inheritance

--- projects/views.py
+++ projects/views.py
@@ -10,24 +10,6 @@ def welcome(request):
 def projects_today(request):
     date = dt.date.today()
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Projects for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)    
+    return render(request, 'all-projects/today-projects.html', {"date":date}) 
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day    
+  
