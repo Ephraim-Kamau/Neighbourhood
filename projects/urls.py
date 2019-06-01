6be384caa5a9efee70b23ab21132136756475785
Ephraim-Kamau <kephraim19@gmail.com>
2019-06-01 14:20:56 +0300

    Add changes to urls

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.projects_today,name='projectsToday')
+    url('^$',views.projects_today,name='projectsToday'),
 
 ]
